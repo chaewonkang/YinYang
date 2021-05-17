6d0c6f9615425ef0252e7fdc38c848fae20d41ed
Chaewon Kang <saturday.the17th@gmail.com>
2021-05-17 23:40:57 +0900

    body overflow-y hidden

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4f3c8b1ddcf268b42e91";
+/******/ 	var hotCurrentHash = "85c6afee78456b02707f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
